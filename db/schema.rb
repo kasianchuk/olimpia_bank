3073b8aea0d678c4773fba6e519d69faa47d95dc
Pavlo Kasianchuk <kasianchuk.pavlo@gmail.com>
2018-06-20 19:42:23 +0300

    Added accounts functionality (basic scaffold)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_162055) do
+ActiveRecord::Schema.define(version: 2018_06_20_164138) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.integer "user_id"
+    t.decimal "balance"
+    t.string "currency"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
