2a91b295fedccbe6a44beaa006070e29bb06f744
Pavlo Kasianchuk <kasianchuk.pavlo@gmail.com>
2018-06-23 15:07:40 +0300

    Make User model non-deletable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :accounts
+  has_many :accounts, dependent: :destroy
+
+  before_destroy :nope
+
+  private
+
+  def nope
+    raise "nope!"
+  end
 end
