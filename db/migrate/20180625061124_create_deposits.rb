eb7c6728ec8f8780522490d62e0bed43ab26813b
Pavlo Kasianchuk <kasianchuk.pavlo@gmail.com>
2018-06-27 03:35:27 +0300

    Remove Models Depposit, Withdraw with migrations

--- db/migrate/20180625061124_create_deposits.rb
+++
@@ -1,9 +0,0 @@
-class CreateDeposits < ActiveRecord::Migration[5.2]
-  def change
-    create_table :deposits do |t|
-      t.decimal :amount
-      t.references :account, foreign_key: true
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20180625061143_create_withdraws.rb
+++
@@ -1,9 +0,0 @@
-class CreateWithdraws < ActiveRecord::Migration[5.2]
-  def change
-    create_table :withdraws do |t|
-      t.decimal :amount
-      t.references :account, foreign_key: true
-      t.timestamps
-    end
-  end
-end
