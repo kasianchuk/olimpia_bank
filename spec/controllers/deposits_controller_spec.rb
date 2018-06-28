e4d6138992201dcb8b51997804b44bb196e0cdbb
Pavlo Kasianchuk <kasianchuk.pavlo@gmail.com>
2018-06-28 21:29:05 +0300

    Delete not needed rspec tests

--- spec/controllers/deposits_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe DepositsController, type: :controller do
-  it "GET new" do
-    get :new, params: {account_id: 1}
-    expect(response.status).to eq(200)
-  end
-end
