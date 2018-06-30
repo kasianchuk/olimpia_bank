6dc8969020c98049a9dd37cb41d891ba9b9d6b86
Pavlo Kasianchuk <kasianchuk.pavlo@gmail.com>
2018-06-30 11:57:51 +0300

    delete duplicate record in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :accounts, dependent: :destroy
-  has_secure_token
+
   has_secure_token :token
 
   before_destroy :nope
