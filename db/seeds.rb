bcda13cbbeadba1ef60bd90745a4e9a9626853d0
Pavlo Kasianchuk <kasianchuk.pavlo@gmail.com>
2018-07-01 23:42:16 +0300

    Move load_database task to db:seed, update it by lineal algoritm

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,33 @@
 #
 # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # Character.create(name: 'Luke', movie: movies.first)
+require 'yaml'
+data = YAML.load_file('config/imported_data.yml')
+user_ids = {}
+account_ids = {}
+
+data['users'].each do |user_id, user_data|
+  user = User.create!(name:  user_data['name'],
+                      email: "#{user_data['name']}@olimpia.com",
+                      password: user_data['password'])
+  user_ids[user_id] = user.id
+end
+
+data['accounts'].each do |account_id, account_data|
+  account = Account.create!(balance:  account_data['balance'],
+                            currency: account_data['currency'],
+                            user_id:  user_ids[account_data['user_id']])
+  account_ids[account_id] = account.id
+end
+
+data['deposits'].each do |_, deposit_data|
+  MoneyOperation.create!(amount:  deposit_data['amount'],
+                         operation: 'deposit',
+                         account_id:  account_ids[deposit_data['account_id']])
+end
+
+data['witdraws'].each do |_, withdraw_data|
+  MoneyOperation.create!(amount:  withdraw_data['amount'],
+                         operation: 'withdraw',
+                         account_id:  account_ids[withdraw_data['account_id']])
+end
