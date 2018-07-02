d50a53ac4f4ce0d7d97497468d88d55ee831196d
Pavlo Kasianchuk <kasianchuk.pavlo@gmail.com>
2018-07-03 00:38:17 +0300

    update initialize params in lib/./account_context.rb

--- lib/services/authorization/account_context.rb
+++ lib/services/authorization/account_context.rb
@@ -3,9 +3,9 @@ module Services
     class AccountContext
       attr_reader :user, :params_user_id
 
-      def initialize(options = {})
-        @user = options.fetch(:current_user)
-        @params_user_id = options.fetch(:params_user_id)
+      def initialize(current_user, params_user_id)
+        @user = current_user
+        @params_user_id = params_user_id
       end
     end
   end
