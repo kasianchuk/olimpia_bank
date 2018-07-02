5e748ab8b29eccfcefb11c5bc52017dfdc0dcd00
Pavlo Kasianchuk <kasianchuk.pavlo@gmail.com>
2018-07-02 14:30:23 +0300

    Update operation_errors class, add call methods

--- lib/services/operation_errors.rb
+++ lib/services/operation_errors.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
 module Services
-  class OperationErrors < Services::OlimpiaBank
+  module OperationErrors
+    CHECK_AMOUNT = 'ERROR: Please enter positive amount'
+    NOT_ENOUGH_MONEY = 'ERROR: There is not enough money at the moment in account'
+
     # The NegativeAmount class check amount.negative?
-    class NegativeAmount < StandardError; end
+    class NegativeAmount < StandardError
+      def self.call
+        CHECK_AMOUNT
+      end
+    end
+
     # The ZeroAmount class check amount.zero?
-    class ZeroAmount < StandardError; end
+    class ZeroAmount < StandardError
+      def self.call
+        CHECK_AMOUNT
+      end
+    end
+
     # The NotEnoughMoney class check user.account.balance
-    class NotEnoughMoney < StandardError; end
+    class NotEnoughMoney < StandardError
+      def self.call
+        NOT_ENOUGH_MONEY
+      end
+    end
   end
 end
